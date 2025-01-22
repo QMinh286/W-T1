466fd83678de4b531fdd37754e0b6e5868c43627
Quang Minh Vu <Qvu5836@conestogac.on.ca>
2025-01-21 19:21:21 -0500

    complete fileHandler.cpp

--- fileHandler.cpp
+++ fileHandler.cpp
@@ -1,2 +1,23 @@
 #include "fileHandler.h"
-	
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <time.h>
+
+static bool LoadFile(string filename, int data[]) {
+
+};
+
+static bool SaveFile(string filename, int data[]) {};
+
+static bool SendFile(string filename, const void* data, int size) {};
+
+static bool ReceiveFile(string filename, void* data, int size) {};
+
+static string CheckTotalData( int originalSize, int receivedSize) {};
+
+static string CheckData( int originalSize, int receivedSize) {};
