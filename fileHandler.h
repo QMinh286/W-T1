25183f26e175afb71cf9bfccb169d794b668d6e1
Quang Minh Vu <Qvu5836@conestogac.on.ca>
2025-01-21 19:18:10 -0500

    complete fileHandler.h

--- fileHandler.h
+++ fileHandler.h
@@ -1,2 +1,13 @@
 #pragma once
-#include ""
+#include "ReliableUDP.cpp"
+class fileHandler
+{
+public:
+	static bool LoadFile(string filename, int data[]);
+	static bool StoreFile(string filename, const int data[]);
+	static bool SendFile(string filename, const void* data, int size);
+	static bool ReceiveFile(string filename, void* data, int size);
+	static string CheckTotalData( int originalSize, int receivedSize);
+	static bool VerifyFile(string filename);
+};
+
